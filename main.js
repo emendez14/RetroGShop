be9113f0b9f58922d02ac16337a09eb776b4bf9b
Andy Mendez <endesnsonjr@gmail.com>
2020-01-07 20:04:05 -0500

    third, mid slideshow, styles

--- main.js
+++ main.js
@@ -1,49 +1,127 @@
+// image slider top of page SELECTORS
 let sliderImages = document.querySelectorAll(".slide"),
     arrowLeft = document.querySelector("#arrow-left"),
     arrowRight = document.querySelector("#arrow-right"),
     current = 0;
+    // menu button SELECTORS
 let menuIcon = document.querySelector(".menu-icon"),
-    subNav = document.querySelector("#sub-nav");
+    subNav = document.querySelector("#sub-nav"),
+    mainText = document.querySelectorAll(".content"),
+    closeBtn = document.querySelector(".exit");
+    // image slider middle of page SELECTORS
+let carouselSlide = document.querySelector(".carousel-slide"),
+    carouselImages = document.querySelectorAll(".carousel-slide img"),
+    prevBtn = document.querySelector("#prevBtn"),
+    nextBtn = document.querySelector("#nextBtn"),
+    counter = 1,
+    size = carouselImages[0].clientWidth;
+    // item sercher SELECTORS
+let systemsCollection = document.querySelectorAll("#collection"),
+    systemsForm = document.querySelector("#item-searcher"),
+    systemsInput = document.querySelector(".systemsInput"),
+    systemsListItems = document.querySelectorAll(".item");
 
+    // menu button LOGIC
 menuIcon.addEventListener("click", function(){
-    
-})
+    subNav.classList.add("open");
+    subNav.style.display = "block";
+    subNav.classList.remove("closed");
+});
 
+closeBtn.addEventListener("click", function(){
+    subNav.classList.add("closed");
+    subNav.style.display = "none";
+    subNav.classList.remove("open");
+});
+
+function loadEventListeners(){
+    systemsInput.addEventListener("keyup", filterSystems)
+}
+
+loadEventListeners();
+
+function filterSystems(e){
+    let text = e.target.value;
+
+    document.querySelectorAll(".item").forEach(function(system){
+        let item = system.firstChild.textContent;
+        if(item.indexOf(text) != -1){
+            system.style.display = "block";
+        } else {
+            system.style.display = "none";
+        }
+    });
+};
+
+    // image slider top LOGIC
 function reset(){
     for(let i = 0; i < sliderImages.length; i++){
         sliderImages[i].style.display = "none";
     }
-}
-
+};
+    // initialize the image slider
 function initializer(){
     reset();
     sliderImages[0].style.display = "block";
-}
+};
 
 function slideLeft(){
     reset();
     sliderImages[current - 1].style.display = "block";
     current--;
-}
+};
 
 arrowLeft.addEventListener("click", function(){
     if(current === 0){
         current = sliderImages.length;
     }
     slideLeft();
-})
+});
 
 function slideRight(){
     reset();
     sliderImages[current + 1].style.display = "block";
     current++;
-}
+};
 
 arrowRight.addEventListener("click", function(){
     if(current === sliderImages.length - 1){
         current = -1;
     }
     slideRight();
-})
-
+});
+    // image slider top should now function
 initializer();
+
+
+    // image slider middle of page LOGIC
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextBtn.addEventListener("click", function(){
+    if (counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+
+prevBtn.addEventListener("click", function(){
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+    // on end of every transition, check for id, if it has the id, return to beginning of HTMLcollection
+carouselSlide.addEventListener("transitionend", function(){
+    if(carouselImages[counter].id === 'lastClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if(carouselImages[counter].id === 'firstClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
